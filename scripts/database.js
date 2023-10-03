674e48014d2102b59816543c769ae51c1f81c36d
Chelsea Hatton <chelseahatton1992@gmail.com>
2023-10-03 11:33:59 -0400

    new main

--- scripts/database.js
+++ scripts/database.js
@@ -6,8 +6,23 @@ const database = {
         { id: 2, name: "Naboo"},
         { id: 3, name: "Central Perk"},
         { id: 4, name: "Starfleet Headquarters"}
-    ]
+    ],
+    // foods: [
+    //     { id: 1, name: "The Mothman", desc: "A hot dog with beans and two pepperoni eyes on top", price: 3, img: ""},
+    //     { id: 2, name: "The Casa Grande Special", desc: "Definitely not just a taco", price: 3.5, img: ""},
+    //     { id: 3, name: "The Kirk Dog", desc: "Ripped shirt not included, but the William Shatner tax is", price: 8, img: ""},
+    //     { id: 4, name: "Hallowieners", desc: "Our spooky special", price: 2, img: ""}
+    // ]
 }
+
+// export const getFoods =() => {
+//     return database.getFoods.map(f => ({...f}))
+// }
+
+// export const setFoods = (foodsId) => {
+//     database.transientState.selectedLocation = locationId
+//     document.dispatchEvent(new CustomEvent("stateChanged"))
+// }
 
 export const setLocation = (locationId) => {
     database.transientState.selectedLocation = locationId
