de7e396d614c16b34843bda5144a0e5b27546a98
Haleigh Smith <haleighbsmiths@gmail.com>
2023-10-03 13:47:07 -0400

    hs database

--- scripts/database.js
+++ scripts/database.js
@@ -1,19 +1,18 @@
 const database = {
-    transientState: {}
+    desserts: [
+        {id: 1, name: "Peppermint Pepperoni Roll", desc: "With real pepperonis!", price: 3, image: "" },
+        {id: 2, name: "Wild Ramp", desc: "Smelly but tasty.", price: 3, image: "" },
+        {id: 3, name: "Beans and Cornbread", desc: "Just like Grandma's house. ", price: 2, image: "" },
+        {id: 4, name: "Coal", desc: "Don't ask questions.", price: 1, image: "" },
+    ]
+}
+export const getDesserts = () => {
+    database.desserts.map(dessert => ({...dessert}))
 }
 
-export const setLocation = (locationId) => {
-    database.transientState.selectedLocation = locationId
+export const setDessert = (dessertId) => {
+    database.transientState.selecteddessert = dessertId
     document.dispatchEvent( new CustomEvent("stateChanged") )
 }
 
-export const getLocations = () => {
-    return database.getLocations.map(f => ({...f}))
-}
 
-export const completeOrder = () => {
-
-        // Broadcast custom event to entire documement so that the
-        // application can re-render and update state
-        document.dispatchEvent( new CustomEvent("stateChanged") )
-}
