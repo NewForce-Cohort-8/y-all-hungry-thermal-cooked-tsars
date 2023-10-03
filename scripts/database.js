cce04bb682970741690fdaaab111cbc21ce4719f
Norwood Henderson <norwoodmhii@gmail.com>
2023-10-03 13:28:51 -0400

    fixed merge conflicts, added funcs to db for toys

--- scripts/database.js
+++ scripts/database.js
@@ -36,21 +36,16 @@ const database = {
         {id: 1, name: "Darmok and Gelatto", desc: "", price: 4, image: "" },
         {id: 1, name: "Romulan Ale", desc: "", price: 1, image: "" },
         {id: 1, name: "Bantha Milk", desc: "", price: 2, image: "" },
-<<<<<<< HEAD
     ],
     
     
-=======
-],
+     foods: [
+     { id: 1, name: "The Mothman", desc: "A hot dog with beans and two pepperoni eyes on top", price: 3, img: ""},
+     { id: 2, name: "The Casa Grande Special", desc: "Definitely not just a taco", price: 3.5, img: ""},
+     { id: 3, name: "The Kirk Dog", desc: "Ripped shirt not included, but the William Shatner tax is", price: 8, img: ""},
+     { id: 4, name: "Hallowieners", desc: "Our spooky special", price: 2, img: ""}
+ ],
 
-//     foods: [
-//     { id: 1, name: "The Mothman", desc: "A hot dog with beans and two pepperoni eyes on top", price: 3, img: ""},
-//     { id: 2, name: "The Casa Grande Special", desc: "Definitely not just a taco", price: 3.5, img: ""},
-//     { id: 3, name: "The Kirk Dog", desc: "Ripped shirt not included, but the William Shatner tax is", price: 8, img: ""},
-//     { id: 4, name: "Hallowieners", desc: "Our spooky special", price: 2, img: ""}
-// ],
-
->>>>>>> main
     locations: [
         { id: 1, name: "Gotham City"},
         { id: 2, name: "Naboo"},
@@ -72,14 +67,14 @@ export const getDrinks = () => {
     database.drinks.map(drink => ({...drink}))
 }
 
-// export const getFoods =() => {
-//     return database.getFoods.map(f => ({...f}))
-// }
+ export const getFoods =() => {
+     return database.getFoods.map(f => ({...f}))
+ }
 
-// export const setFoods = (foodsId) => {
-//     database.transientState.selectedLocation = locationId
-//     document.dispatchEvent(new CustomEvent("stateChanged"))
-// }
+ export const setFoods = (foodsId) => {
+     database.transientState.selectedLocation = locationId
+     document.dispatchEvent(new CustomEvent("stateChanged"))
+ }
 
 export const setLocation = (locationId) => {
     database.transientState.selectedLocation = locationId
