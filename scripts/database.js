60cb68836370eea99fc169bc50ec25d6744c2b77
Haleigh Smith <haleighbsmiths@gmail.com>
2023-10-03 13:57:32 -0400

    resolved merge conflicts

--- scripts/database.js
+++ scripts/database.js
@@ -1,23 +1,15 @@
 const database = {
-<<<<<<< HEAD
+    
+
+
+
+    transientState: {},
     desserts: [
         {id: 1, name: "Peppermint Pepperoni Roll", desc: "With real pepperonis!", price: 3, image: "" },
         {id: 2, name: "Wild Ramp", desc: "Smelly but tasty.", price: 3, image: "" },
         {id: 3, name: "Beans and Cornbread", desc: "Just like Grandma's house. ", price: 2, image: "" },
         {id: 4, name: "Coal", desc: "Don't ask questions.", price: 1, image: "" },
-    ]
-}
-export const getDesserts = () => {
-    database.desserts.map(dessert => ({...dessert}))
-}
-
-export const setDessert = (dessertId) => {
-    database.transientState.selecteddessert = dessertId
-    document.dispatchEvent( new CustomEvent("stateChanged") )
-}
-
-=======
-    transientState: {},
+    ],
 
     toys: [
         {
@@ -102,5 +94,12 @@ export const setLocation = (locationId) => {
 export const getLocations = () => {
     return database.locations.map(f => ({...f}))
 }
->>>>>>> main
+export const getDesserts = () => {
+    database.desserts.map(dessert => ({...dessert}))
+}
+
+export const setDessert = (dessertId) => {
+    database.transientState.selecteddessert = dessertId
+    document.dispatchEvent( new CustomEvent("stateChanged") )
+}
 
