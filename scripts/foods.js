74b3c770665d68c5a52e14a96263dd0b6846976e
Chelsea Hatton <chelseahatton1992@gmail.com>
2023-10-04 13:35:22 -0400

    new main

--- scripts/foods.js
+++ scripts/foods.js
@@ -1,47 +1,27 @@
 import { getFoods, setFoods } from "./database.js"
 
-const foods = getFoods()
+const foods = getFoods();
 
 document.addEventListener(
     "change",
     (event) => {
         if (event.target.name === "foods") {
-            setFoods(parseInt(event.target.value))
+            setFoods(parseInt(event.target.value));
         }
     }
-)
+);
 
 export const Foods = () => {
-    let html = `div class = "-"
+    let html = `<div class = "-">
     <h2>Food Options</h2>
-    <select id = "int">
-    <option value = "0">Select your hawt dog</option>`
+    <select id = "food-select" name = "foods">
+    <option value = "0">Select your hawt dog</option>`;
 
     for (const food of foods) {
-        html += `option value = "${foods.id}">${foods.name}<option>`
+        html += `option value = "${food.id}">${food.name}<option>`;
     }
 
     html += `</select>
     </div>`
-    return html
+    return html;
 }
-
-document.addEventListener(
-    "change",
-    (changeEvent) => {
-        if (changeEvent.target.id === "foods")) {
-const buildOrderListItem = (order) => {
-    const foods = getFoods()
-}
-}
-
-
-document.addEventListener(
-    "change",
-    (changeEvent) => {
-        if (changeEvent.target.id === "resource") {
-            const chosenOption = changeEvent.target.value
-            console.log(chosenOption)
-        }
-    }
-)
