8c38ab759a1bfe1f0939cfaecafa8e7559b7c0fd
Norwood Henderson <norwoodmhii@gmail.com>
2023-10-06 11:39:25 -0400

    fixed curly bracket issues, needed to add one on select drinks js

--- scripts/selectDrink.js
+++ scripts/selectDrink.js
@@ -4,9 +4,10 @@ export const selectDrink = () => {
     const drinks = getDrinks()
     const state = getTransientState()
 
-    for(const singleDrink of drinks)
+    for(const singleDrink of drinks){
 
     if(state.selectedDrink === singleDrink.id){
         return `<h1>${singleDrink.name}</h1>`
-    }
+      }
+   }
 }
